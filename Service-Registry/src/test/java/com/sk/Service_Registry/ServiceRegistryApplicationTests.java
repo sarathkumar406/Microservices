ed6a9fe4ddb55434856e763bddc37baa21c2e7f1
Sarath <sarathkumar.406@gmail.com>
2025-04-19 22:27:39 +0530

    Service Registry Microservices

---
+++ Service-Registry/src/test/java/com/sk/Service_Registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.Service_Registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
